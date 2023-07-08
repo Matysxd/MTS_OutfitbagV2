cec735da8c29aa515e384fb124bd2cc0e95807c5
Matysxd <118368403+Matysxd@users.noreply.github.com>
2023-07-08 02:30:35 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Item = 'outfitbag' -- Name of your item
+
+Config.OpenDelay = '10' -- How long you have to wait before opening the bag again in minutes
+Config.OpenWard = 'fivem-appearance:pickNewOutfit' -- Your event to open wardrobe
+
+Config.Lang = {
+    TakeBag = 'Take outfitbag',
+    OpenWard = 'Open outfitbag',
+    Place = 'Placing outfitbag',
+    Taking = 'Picking up outfitbag'
+}
